919c64703b1b82f89ef7a97b196d5cdd2901e4bd
Lawted <wumingze@minimaxi.com>
2022-12-06 19:57:47 +0800

    feat: finish 2a

--- codes/ex2a_blank.py
+++ codes/ex2a_blank.py
@@ -7,21 +7,32 @@ import cv2
 # NO OPENCV FUNCTION IS ALLOWED HERE
 
 
+# Implement gaussian filtering using a grayscale lena.jpg as an input.
+# See the results with varying the window size, W=5,11, and the standard deviation, σ_s=1,10 .
 
+def gaussian_kernel(W, sigma):
+    k = np.zeros((W, W))
+    for i in range(W):
+        for j in range(W):
+            k[i, j] = np.exp(-((i-W//2)**2 + (j-W//2)**2) / (2*sigma**2))
+    k = k / np.sum(k)
+    return k
 
-
-
-
-
-
-
-
-
+def gaussian_filter_gray(img, W, sigma):
+    # W: window size
+    # sigma: standard deviation
+    # img: input image
+    # img_out: output image
+    # YOUR CODE HERE
+    img_out = np.zeros(img.shape)
+    img_pad = np.pad(img, ((W//2, W//2), (W//2, W//2)), 'edge')
+    for i in range(img.shape[0]):
+        for j in range(img.shape[1]):
+            img_out[i, j] = np.sum(img_pad[i:i+W, j:j+W] * gaussian_kernel(W, sigma))
+    return img_out
 
 
 ##########################################################################################
-
-
 im_gray = cv2.imread('../inputs/lena.jpg', 0)
 im_gray = cv2.resize(im_gray, (256, 256))
 
